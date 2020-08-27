46cd3bd325197d4b8cd8d195097fa17798f50fdf
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-08-27 09:31:34 -0300

    Update: Time Package

--- src/Time/LocalTimeConverter.java
+++ src/Time/LocalTimeConverter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Utilities.Time;
+package Time;
 
 import java.time.LocalTime;
 import java.time.ZoneId;
@@ -15,16 +15,22 @@ import java.util.Date;
  * @author Samuel
  */
 public class LocalTimeConverter {
-    
-    private final DateTimeFormatter DEFAULT_FORMAT = DateTimeFormatter.ofPattern("hh : mm :ss");
-    private final DateTimeFormatter SHORT_FORMAT = DateTimeFormatter.ofPattern("hh : mm");
-    
-    
+
+    private final DateTimeFormatter DEFAULT_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private final DateTimeFormatter SHORT_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
+
     // LocalTime
-    
-    public LocalTime toLocalTime(String string){
-        
-      return LocalTime.parse(string, DEFAULT_FORMAT);
+    public LocalTime toLocalTime(String string) {
+
+        try{
+            
+        String time = string.substring(string.indexOf(" ")+1,string.length());    
+            
+            return LocalTime.parse(time, SHORT_FORMAT);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+            return null;
+        }
     }
 
     public LocalTime fromDate(Date date) {
@@ -33,20 +39,17 @@ public class LocalTimeConverter {
 
         return local;
     }
-    
+
     // SQL
-    
     public java.sql.Time toSQLTime(LocalTime localTime) {
-    
+
         return java.sql.Time.valueOf(localTime);
     }
-    
+
     // Strings
-    
     public String shortFormat(LocalTime localTime) {
-    
-     // return localTime.getHour()+" : "+localTime.getMinute();
-     
-      return SHORT_FORMAT.format(localTime);
+
+        // return localTime.getHour()+" : "+localTime.getMinute();
+        return SHORT_FORMAT.format(localTime);
     }
 }
