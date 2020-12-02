59c3290dfb9c230f3d501daae2bda85e183a089f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-02 15:29:14 -0300

    Created: Function - public void download(InputStream input, File destiny) (Downloader)

--- src/Services/Downloader.java
+++ src/Services/Downloader.java
@@ -5,6 +5,15 @@
  */
 package Services;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Samuel
@@ -26,5 +35,27 @@ public class Downloader extends Thread{
         
     }
     
-    
+    public void download(InputStream input, File destiny){
+        
+        try {
+        
+            OutputStream output = new FileOutputStream(destiny);
+      
+            int bit = 0;
+            
+            while((bit = input.read()) > -1){
+                
+                output.write(bit);
+            }
+        
+            input.close();
+            output.close();
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Downloader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Downloader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
